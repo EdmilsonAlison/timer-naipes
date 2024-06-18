b42cf5e7eac5f22fc8607d4e062e2858aa002181
Eddie Dias <eddias.mazor@gmail.com>
2024-06-18 12:44:41 -0300

    criação e finalização do component Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -15,6 +15,7 @@ export const Header = () => {
                 <NavLink to="/history" title={"History"}>
                     <Scroll size={24}/>
                 </NavLink>
+
             </nav>
         </HeaderContainer>
     );
